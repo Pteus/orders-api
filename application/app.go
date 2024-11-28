f6fc8409b3a49ec6ae02a5668993d4dcc1f4a1d9
Pedro Mateus <pedropmateus91@gmail.com>
2024-11-28 16:53:46 +0000

    add redis, gracefull shutdown

--- application/app.go
+++ application/app.go
@@ -4,16 +4,20 @@ import (
 	"context"
 	"fmt"
 	"github-com/pteus/orders-api/middleware"
+	"github.com/redis/go-redis/v9"
 	"net/http"
+	"time"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func NewApp() *App {
 	return &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 }
 
@@ -23,10 +27,38 @@ func (a *App) Start(ctx context.Context) error {
 		Handler: middleware.Logging(a.router),
 	}
 
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(ctx).Err()
 	if err != nil {
-		return fmt.Errorf("could not start server: %w", err)
+		return fmt.Errorf("could not connect to redis: %w", err)
 	}
 
-	return nil
+	fmt.Println("starting server...")
+
+	ch := make(chan error, 1)
+
+	go func() {
+		err = server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("could not start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("could not close redis connection:", err)
+		}
+	}()
+
+	fmt.Println("listening on port 3000")
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancelFunc()
+
+		return server.Shutdown(timeout)
+	}
 }
--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"fmt"
 	"github-com/pteus/orders-api/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.NewApp()
 
-	err := app.Start(context.TODO())
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop() // defers the execution of stop() until the surrounding function returns
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
-
 }
